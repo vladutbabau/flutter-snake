2ae0b3f90926c6e57d3cfbb09a289cd14a29c7f0
Băbău Vlad - Andrei <vladut_babau@yahoo.com>
2024-01-25 16:38:31 +0200

    Remove Firebase implementation attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'home_page.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
